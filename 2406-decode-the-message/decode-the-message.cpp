00fabed3ce225f2eaac867806956632e048811ad
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-24 00:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (81.02%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        char start ='a';
+        char mapping[300] = {0};
+
+        for(auto ch : key){
+            if(ch != ' ' && mapping[ch]== 0 ){
+                mapping[ch] = start;
+                start++;
+            }
+        }
+
+
+        string ans;
+        for(auto ch: message){
+            if(ch == ' '){
+                ans.push_back(' ');
+            }else{
+
+            char decode = mapping[ch];
+            ans.push_back(decode);
+            }
+        }
+        
+        return ans;
+    }
+};
