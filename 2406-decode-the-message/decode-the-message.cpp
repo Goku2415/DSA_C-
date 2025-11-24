5fabafd87599e600174d419abb7a83ddf5f635b3
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-24 15:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.63%) - LeetSync

--- 2406-decode-the-message/decode-the-message.cpp
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -1,28 +1,65 @@
+// class Solution {
+// public:
+//     string decodeMessage(string key, string message) {
+//         char start ='a';
+//         char mapping[300] = {0};
+
+//         for(auto ch : key){
+//             if(ch != ' ' && mapping[ch]== 0 ){
+//                 mapping[ch] = start;
+//                 start++;
+//             }
+//         }
+
+
+//         string ans;
+//         for(auto ch: message){
+//             if(ch == ' '){
+//                 ans.push_back(' ');
+//             }else{
+
+//             char decode = mapping[ch];
+//             ans.push_back(decode);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+//Hashmap method
+
+
+
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        char start ='a';
-        char mapping[300] = {0};
+        unordered_map<char, char> mp;   // HASHMAP
+        char start = 'a';
 
-        for(auto ch : key){
-            if(ch != ' ' && mapping[ch]== 0 ){
-                mapping[ch] = start;
-                start++;
+        // Build substitution mapping
+        for (char ch : key) {
+            if (ch != ' ' && mp.find(ch) == mp.end()) {
+                mp[ch] = start++;
             }
         }
 
-
+        // Decode the message
         string ans;
-        for(auto ch: message){
-            if(ch == ' '){
+        for (char ch : message) {
+            if (ch == ' ') {
                 ans.push_back(' ');
-            }else{
-
-            char decode = mapping[ch];
-            ans.push_back(decode);
+            } else {
+                ans.push_back(mp[ch]);
             }
         }
-        
+
         return ans;
     }
 };
