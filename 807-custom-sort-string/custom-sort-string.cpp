0f20576695df67cb8e1acd2e4a34a3f4598ab7ec
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-26 16:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (89.96%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+       // custom comparator
+    static string str;
+
+    static bool compare(char char1, char char2) {
+    // this will return true if position of char1 in str string
+    // is less than the position of char2 in str string
+    // when true is returned, then char1 will be placed before char2
+    // in output string
+    return (str.find(char1) < str.find(char2));
+    }
+    string customSortString(string order, string s) {
+    str = order;
+    sort(s.begin(), s.end(), compare);
+    return s;
+    }
+};
+    string Solution::str;
