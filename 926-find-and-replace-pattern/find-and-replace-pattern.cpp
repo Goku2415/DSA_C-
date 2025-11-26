6d7d2e83bc2b1bd46263b7851173b8eff2342b2b
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-27 00:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (90.78%) - LeetSync

---
+++ 926-find-and-replace-pattern/find-and-replace-pattern.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void createMapping(string& str){
+
+        char start = 'a';
+        char mapping[300] = {0};
+
+        for (auto ch : str) {
+            if (mapping[ch] == 0) {
+            mapping[ch] = start;
+            start++;
+            }
+        }
+        // update the string
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str[i];
+            str[i] = mapping[ch];
+        }
+    }
+    
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+
+
+        vector<string> ans;
+
+        // firstly normalise the pattern
+        createMapping(pattern);
+
+        // saare words k sath khelna h
+        for (string s : words) {
+            string tempString = s;
+
+            // normalise tempString
+            createMapping(tempString);
+
+            if (tempString == pattern) {
+            // it means that "s" wali string was a valid answer
+            ans.push_back(s);
+            }
+
+        }
+
+    return ans;
+    }
+};
