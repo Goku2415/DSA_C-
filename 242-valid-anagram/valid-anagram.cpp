f08e527d1abcae97a6e87032771db8d8f1c3ed76
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-28 23:21:03 +0530

    Time: 8 ms (7.91%) | Memory: 9.7 MB (73.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         int i=0;
-        while(i<t.size() && i<s.size()){
+        while(i<t.size() ){
             
             if(s[i]!=t[i] || s.size() !=t.size()){
                 return false;
