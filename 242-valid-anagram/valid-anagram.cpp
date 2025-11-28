d114d6a3d0c638c041fd3887b1960aa047642180
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-11-29 00:13:22 +0530

    Time: 7 ms (20.42%) | Memory: 9.7 MB (73.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+
+    
+    //Method 1
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         int i=0;
-        while(i<t.size() ){
-            char ans= s[i]^t[i];
-            if(ans!= 0 || s.size()!=t.size()){
+        while(i<t.size() ){          
+            if(s[i]!=t[i] || s.size() !=t.size()){
                 return false;
-            }
-            
-            // if(s[i]!=t[i] || s.size() !=t.size()){
-            //     return false;
-            // } 
-            i++;
-           
+            } 
+            i++;   
         }
         return true;
 
     }
+
+
+    //Method 2
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     int i=0;
+    //     while(i<t.size() ){
+    //         char ans= s[i]^t[i];
+    //         if(ans!= 0 || s.size()!=t.size()){
+    //             return false;
+    //         }
+    //         i++;
+           
+    //     }
+    //     return true;
+
+    // }
 };
