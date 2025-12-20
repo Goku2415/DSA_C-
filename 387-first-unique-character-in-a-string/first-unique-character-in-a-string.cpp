93c004ac05585d623b5b9907b9c07352b9e2a739
Mahesh Singh <maheshsinghgaria24@gmail.com>
2025-12-20 09:22:37 +0530

    Time: 3 ms (80.65%) | Memory: 14.7 MB (73.00%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -7,11 +7,8 @@ public:
             freq[c]++; 
             
         }
-        for(int i=0;i<s.length();i++){
-            if(freq[s[i]]==1){
-                return i;
-            }
-
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]]==1) return i;
         }
         return -1;
 
