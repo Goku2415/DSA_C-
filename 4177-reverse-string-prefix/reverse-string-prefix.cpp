1f30e924f8b3969cda9f0a325adbf20eaf850fca
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-01-11 14:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (56.73%) - LeetSync

---
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+
+        for(int i=0,j=k-1;  i<=j; ){
+            swap(s[i++],s[j--]);
+            
+        }
+        
+        return s;
+        
+    }
+};
