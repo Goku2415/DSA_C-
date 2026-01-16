44c14ae4871dee0217388de7e03696eb0a82c295
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-01-16 15:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (6.68%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -10,13 +10,9 @@ public:
             if(i<nums.size()){
                 sum+=nums[i-1];
             }
-            // cout<<"This is the summm "<<sum<<endl;
             Lans.push_back(sum);
             i++;
-            // cout<<"This is the array element"<<Lans<<endl;
         }
-        for(auto a:Lans)cout<<"This is the array after reverse"<<a<<endl;
-
         sum=0;
 
         int j=nums.size()-1;
@@ -25,18 +21,13 @@ public:
             if(j >=0){
                 sum+=nums[j+1];
             }
-            // cout<<"THis is the sum2  "<<sum<<endl;
-            Rans.push_back(sum);
-            // cout<<"this is the array element"<<Rans<<endl;
+        Rans.push_back(sum);
         }
         
         reverse(Rans.begin(),Rans.end());
-        for(auto a:Rans)cout<<"This is the array after reverse"<<a<<endl;
-
+        
         for(int i=0,j=0;i<nums.size();i++,j++){
             nums[i] = abs(Lans[i]-Rans[j]);
-            cout<<"this is the final array elements"<<nums[i]<<endl;
-
         }
         return nums;
     }
