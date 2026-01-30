2a8d4bfefcb8f6cc7a92c6f8e1dfa065e05cfda3
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-01-30 08:29:07 +0530

    Time: 19 ms (0.56%) | Memory: 16.2 MB (16.28%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
 
-    bool isSafe(vector<vector<int>>& mat,vector<vector<int>> &ans, int& i, int& j ){
-        cout<<"inside the safe function "<<ans[i][j]<<endl;
+    // bool isSafe(vector<vector<int>>& mat,vector<vector<int>> &ans, int& i, int& j ){
+    //     cout<<"inside the safe function "<<ans[i][j]<<endl;
 
-        if(i==j && ans[i][j]==0){
-            return true;
-        }
-        if(ans[i][j]==0){
-            return true;
-        }
-        return false;
-    }
+    //     if(i==j && ans[i][j]==0){
+    //         return true;
+    //     }
+    //     if(ans[i][j]==0){
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     int diagonalSum(vector<vector<int>>& mat) {
         
@@ -22,11 +22,11 @@ public:
         
         while(i<row && j<col){
             cout<<"loop1 ele: "<<mat[i][j]<<endl;
-            if(isSafe(mat,ans,i,j)){
+            // if(isSafe(mat,ans,i,j)){
                 sum+= mat[i][j];
                 cout<<"this is sum"<<sum<<endl;
                 ans[i][j]=1;
-            }
+            // }
             i++;j++;
         }
 
@@ -34,14 +34,19 @@ public:
         while(k<row && l>=0){
             cout<<"loop2 ele: "<<mat[k][l]<<endl;
 
-            if(isSafe(mat,ans,k,l)){
+            // if(isSafe(mat,ans,k,l)){
                 sum+=mat[k][l];
                 cout<<"This is sum inside 2nd loop"<<sum<<endl;
                 ans[k][l]=1;
-            }
+            // }
             k++;l--;
 
         }
+        if(row%2!=0){
+            int h = (row-1)/2;
+            sum-= mat[h][h];
+        }
+
 
 
         return sum;
