b4e7ce42cfc8cdfae2bdca4556054c872ac453f6
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-02 13:00:05 +0530

    Time: 2 ms (62.07%) | Memory: 12.2 MB (11.40%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -9,21 +9,16 @@ public:
         int i = 0;
         while(i<n){
             freq1[ransomNote[i]]++;
-            cout<<"freq of ransomNote element: "<<ransomNote[i] <<" "<<freq1[ransomNote[i]]<<endl;
             i++;
         }
 
         i=0;    
         while(i<m){
             freq2[magazine[i]]++;
-            cout<<"freq of magazine element: "<<magazine[i]<<" "<<freq2[magazine[i]]<<endl;
                i++;
         }
 
         for(auto ch : ransomNote){
-            cout<<"inside for "<<ch<<endl;
-            cout<<"freq1 ele "<<freq1[ch]<<endl;
-            cout<<"freq2 ele "<<freq2[ch]<<endl;
             if(freq2[ch]< freq1[ch]){
                 return false;
             }
