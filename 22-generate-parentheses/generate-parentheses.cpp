adb5fbcc444a9313164702a30cc3e72316a63642
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-03 11:08:13 +0530

    Time: 4 ms (33.98%) | Memory: 16.4 MB (17.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<string>& ans, int n, int open, int close, string output) {
+    void solve(vector<string>& ans, int open, int close, string output) {
         // base case
         if (open == 0 && close == 0) {
             ans.push_back(output);
@@ -10,7 +10,7 @@ public:
         // include open bracket
         if (open > 0) {
             output.push_back('(');
-            solve(ans, n, open - 1, close, output);
+            solve(ans, open - 1, close, output);
             // backtrack
             output.pop_back();
         }
@@ -18,7 +18,7 @@ public:
         // include close bracket
         if (close > open) {
             output.push_back(')');
-            solve(ans, n, open, close - 1, output);
+            solve(ans, open, close - 1, output);
             // backtrack
             output.pop_back();
         }
@@ -29,7 +29,7 @@ public:
         int open = n;
         int close = n;
         string output = "";
-        solve(ans, n, open, close, output);
+        solve(ans, open, close, output);
         return ans;
     }
 };
