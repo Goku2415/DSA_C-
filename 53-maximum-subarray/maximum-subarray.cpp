de624d8ce245e09965af210d3c3838a101fc2d6e
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-13 14:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (15.34%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,11 +4,11 @@ public:
         int curelement =0;
         int maxi = INT_MIN;
         
-        if(nums.size()<1){
-            if(nums[0]>0){
-                return nums[0];
-            }
-        }
+        // if(nums.size()<1){
+        //     if(nums[0]>0){
+        //         return nums[0];
+        //     }
+        // }
 
         for(int i=0;i<nums.size();i++){
             curelement= max(nums[i], curelement + nums[i]);
