48bd164d2bcab4f578066bc819790f7be368636d
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-20 21:04:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (24.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,19 +3,19 @@ public:
 
     
     //Method 1
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        int i=0;
-        while(i<t.size() ){          
-            if(s[i]!=t[i] || s.size() !=t.size()){
-                return false;
-            } 
-            i++;   
-        }
-        return true;
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     int i=0;
+    //     while(i<t.size() ){          
+    //         if(s[i]!=t[i] || s.size() !=t.size()){
+    //             return false;
+    //         } 
+    //         i++;   
+    //     }
+    //     return true;
 
-    }
+    // }
 
 
     //Method 2
@@ -29,9 +29,25 @@ public:
     //             return false;
     //         }
     //         i++;
-           
     //     }
     //     return true;
-
     // }
+
+    //Method 3
+    
+    bool isAnagram(string s, string t) {
+        int freqtable[256]={0};
+        for(int i=0; i<s.size();i++){
+            freqtable[s[i]]++;
+        }
+        for(int i=0; i<t.size();i++){
+            freqtable[t[i]]--;
+        }
+        for(int i=0; i<256;i++){
+            if(freqtable[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
 };
