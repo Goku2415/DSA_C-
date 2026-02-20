e8586f18118082f69b6022de75e1213e2fce6817
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-20 16:22:44 +0530

    Time: 11 ms (5.24%) | Memory: 17.3 MB (55.97%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -2,15 +2,29 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
-        if (k == 1) return 0;
+        // if (k == 1) return 0;
 
-        sort(nums.begin(), nums.end());
-        int ans = INT_MAX;
-        for(auto a:nums)cout<<a<<" ";
+        // sort(nums.begin(), nums.end());
+        // int ans = INT_MAX;
+        // for(auto a:nums)cout<<a<<" ";
 
-        for (int i = 0; i + k - 1 < nums.size(); i++) {
-            ans = min(ans, nums[i + k - 1] - nums[i]);
+        // for (int i = 0; i + k - 1 < nums.size(); i++) {
+        //     ans = min(ans, nums[i + k - 1] - nums[i]);
+        // }
+        // return ans;
+
+
+        sort(nums.begin(),nums.end());
+        if(k==1)return 0;
+        
+        int mini = INT_MAX;
+        for(int i=0; i+k-1<nums.size(); i++){
+            mini= min(mini,nums[i+k-1]-nums[i]);
         }
-        return ans;
+        return mini;
+
+
+
+
     }
 };
