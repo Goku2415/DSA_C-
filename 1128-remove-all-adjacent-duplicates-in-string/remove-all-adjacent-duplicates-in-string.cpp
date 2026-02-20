c0d7b8065c53252e0092c5bfeaf5b41315bda01f
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-02-20 22:34:42 +0530

    Time: 4 ms (67.85%) | Memory: 14.3 MB (32.24%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -4,21 +4,42 @@ public:
         int n = s.length();
         string k;
         int i=0;
+
+        stack<char>st;
         
         while(i<n){
-            if(i==0){
-                k.push_back(s[i++]);
-            }else if(k.length()>0 && s[i]==k.back()  ){
-                k.pop_back();
+            if(!st.empty() && st.top()==s[i]){
+                st.pop();
+                i++;
+            }else{
+                st.push(s[i]);
                 i++;
             }
-            else {
-                k.push_back(s[i++]);
-                
-            }
-            
         }
-        
+
+        while(!st.empty()){
+            k+=st.top();
+            st.pop();
+        }
+        reverse(k.begin(),k.end());
         return k;
+
+
+
+
+
     }
 };
+        //This was solved by me, just remember to not access the k string when nothing is inside the string
+        // while(i<n){
+        //     if(i==0){
+        //         k.push_back(s[i++]);
+        //     }else if(k.length()>0 && s[i]==k.back()  ){
+        //         k.pop_back();
+        //         i++;
+        //     }
+        //     else {
+        //         k.push_back(s[i++]);
+        //     }
+        // }
+        // return k;
