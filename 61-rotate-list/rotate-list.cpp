7f52ebd6ba5f6a894021eb9f0333759005149f68
Mahesh Singh <maheshsinghgaria24@gmail.com>
2026-04-21 23:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.55%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -18,7 +18,7 @@ public:
         ListNode* first = head;
 
         if(head == NULL || size(head) == 0 || k%size(head) == 0  ) return head;
-        if(k==0 || size(head)==1 ) return head;
+        // if(k==0 || size(head)==1 ) return head;
 
         int pos = size(head) - k%size(head);
 
